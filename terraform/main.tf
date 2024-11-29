725d02e6e4014085c0e471c830ea2a7a936e9253
saadrehman171000 <saadrehman1710000@gmail.com>
2024-11-30 02:33:39 +0500

    Update CI/CD configuration for Kubernetes

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,20 +13,23 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 
   # For development/testing only
-  insecure = true
+  tls_insecure_skip_verify = true
 }
 
 variable "host" {
   type = string
+  description = "Kubernetes API endpoint"
 }
 
 variable "token" {
   type = string
   sensitive = true
+  description = "Kubernetes service account token"
 }
 
 variable "cluster_ca_certificate" {
   type = string
+  description = "Kubernetes cluster CA certificate"
 }
 
 resource "kubernetes_namespace" "app_namespace" {
